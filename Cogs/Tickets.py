73228abc524fac16bcc0e13d9ca9a364d10b5586
HeroicosHM <houghtonawe@gmail.com>
2019-06-19 10:53:23 -0700

    initial commit

---
+++ Cogs/Tickets.py
@@ -0,0 +1,363 @@
+import discord
+from discord.ext import commands
+import datetime
+import yaml
+import json
+import re
+import string
+import os
+import random
+import asyncio
+
+class TicketsCog(commands.Cog, name = ":tickets: Tickets (Server Only)"):
+    def __init__(self, bot, support_channel):
+        self.bot = bot
+        print("Loaded TicketsCog.")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if self.bot.log_tickets and not message.author.bot:
+            for channel in self.bot.ticket_data:
+                if channel['ID'] == message.channel.id:
+                    datetime_obj = message.created_at - datetime.timedelta(hours=4)
+                    with open(self.bot.ticket_folder + "/{}.txt".format(str(message.channel.id)), "a+") as file:
+                        file.write("[{} EST] {}: {}\n".format(datetime_obj.strftime("%b-%d-%Y | %X"), message.author.name, message.content))
+
+    def is_support_channel(id):
+        async def check_channel(ctx):
+            return ctx.channel.id == id
+        return commands.check(check_channel)
+
+    @commands.command(name='new', help = 'Open a new support ticket - please include a reason.')
+    @commands.guild_only()
+    @is_support_channel(support_channel)
+    async def new_ticket(self, ctx, *, reason = "No Reason"):
+        print("{} is creating a ticket for {}".format(ctx.author.name, reason))
+
+        await ctx.message.delete()
+
+        await ctx.trigger_typing()
+        found = False
+        for role in ctx.guild.roles:
+            if role.name == self.bot.support_role_name:
+                found = True
+
+        if found == False:
+            if self.bot.use_timestamp:
+                embed = discord.Embed(
+                    title = "Failed to Create Ticket",
+                    description = "This server does not have a `{}` role set up. Please contact a server administrator to solve this.".format(self.bot.support_role_name),
+                    color = random.choice(self.bot.embed_colors),
+                    timestamp = datetime.datetime.now(datetime.timezone.utc)
+                )
+            else:
+                embed = discord.Embed(
+                    title = "Failed to Create Ticket",
+                    description = "This server does not have a `{}` role set up. Please contact a server administrator to solve this.".format(self.bot.support_role_name),
+                    color = random.choice(self.bot.embed_colors)
+                )
+            if self.bot.show_command_author:
+                embed.set_author(
+                    name = ctx.author.name,
+                    icon_url = ctx.author.avatar_url
+                )
+            embed.set_footer(
+                text = self.bot.footer_text,
+                icon_url = self.bot.footer_icon
+            )
+            msg = await ctx.send(embed = embed)
+        else:
+            already_exists = False
+            for channel in self.bot.ticket_data:
+                if channel['Creator'] == str(ctx.author):
+                    already_exists = True
+                    channel_ob = self.bot.get_channel(channel['ID'])
+                    mention = channel_ob.mention
+
+            if already_exists == True:
+                if self.bot.use_timestamp:
+                    embed = discord.Embed(
+                        title = "Failed to Create Ticket",
+                        description = "You already have a ticket open. Please use `{prefix}close` in the {mention} channel before opening a new ticket.".format(prefix=self.bot.prefix, mention=mention),
+                        color = random.choice(self.bot.embed_colors),
+                        timestamp = datetime.datetime.now(datetime.timezone.utc)
+                    )
+                else:
+                    embed = discord.Embed(
+                        title = "Failed to Create Ticket",
+                        description = "You already have a ticket open. Please use `{prefix}close` in the {mention} channel before opening a new ticket.".format(prefix=self.bot.prefix, mention=mention),
+                        color = random.choice(self.bot.embed_colors)
+                    )
+                if self.bot.show_command_author:
+                    embed.set_author(
+                        name = ctx.author.name,
+                        icon_url = ctx.author.avatar_url
+                    )
+                embed.set_footer(
+                    text = self.bot.footer_text,
+                    icon_url = self.bot.footer_icon
+                )
+                msg = await ctx.send(embed = embed)
+            else:
+                support_role = discord.utils.get(ctx.guild.roles, name = self.bot.support_role_name)
+                everyone_perms = discord.PermissionOverwrite(read_messages = False)
+                viewer_perms = discord.PermissionOverwrite(read_messages = True)
+                overwrites = {
+                    ctx.guild.default_role: discord.PermissionOverwrite(read_messages = False),
+                    ctx.guild.me: discord.PermissionOverwrite(read_messages = True),
+                    support_role: discord.PermissionOverwrite(read_messages = True),
+                    ctx.author: discord.PermissionOverwrite(read_messages = True)
+                }
+                ch = await ctx.guild.create_text_channel('ticket-{}'.format(ctx.author.id), overwrites = overwrites, topic = "Reason: " + reason)
+                self.bot.ticket_data.append({"Creator": str(ctx.author), "ID": ch.id, "UserID": ctx.author.id})
+                with open(self.bot.ticket_file, 'w+') as file:
+                    file.write(json.dumps(self.bot.ticket_data, indent = 4, sort_keys = True))
+
+                if self.bot.use_timestamp:
+                    embed = discord.Embed(
+                        title = "New Ticket",
+                        color = random.choice(self.bot.embed_colors),
+                        timestamp = datetime.datetime.now(datetime.timezone.utc)
+                    )
+                else:
+                    embed = discord.Embed(
+                        title = "New Ticket",
+                        color = random.choice(self.bot.embed_colors)
+                    )
+                embed.add_field(
+                    name = "Reason for Ticket:",
+                    value = reason,
+                    inline = False
+                )
+                embed.set_author(
+                    name = ctx.author.name,
+                    icon_url = ctx.author.avatar_url
+                )
+                embed.set_footer(
+                    text = self.bot.footer_text,
+                    icon_url = self.bot.footer_icon
+                )
+                msg = await ch.send(embed = embed, content = support_role.mention + " " + ctx.author.mention)
+
+                if self.bot.use_timestamp:
+                    embed = discord.Embed(
+                        title = "Ticket Created",
+                        description = "Please head to the {} channel to continue.".format(ch.mention),
+                        color = random.choice(self.bot.embed_colors),
+                        timestamp = datetime.datetime.now(datetime.timezone.utc)
+                    )
+                else:
+                    embed = discord.Embed(
+                        title = "Ticket Created",
+                        description = "Please head to the {} channel to continue.".format(ch.mention),
+                        color = random.choice(self.bot.embed_colors)
+                    )
+                if self.bot.show_command_author:
+                    embed.set_author(
+                        name = ctx.author.name,
+                        icon_url = ctx.author.avatar_url
+                    )
+                embed.set_footer(
+                    text = self.bot.footer_text,
+                    icon_url = self.bot.footer_icon
+                )
+                msg = await ctx.send(embed = embed)
+
+            await asyncio.sleep(15)
+            await msg.delete()
+
+    @commands.command(name = 'close', help = 'Close an open support ticket - use in a ticket channel')
+    @commands.guild_only()
+    async def close_ticket(self, ctx):
+        print("{} is trying to close the ticket {}.".format(ctx.author.name, ctx.channel.name))
+
+        if self.bot.use_timestamp:
+            await ctx.message.delete()
+
+        okay_to_delete = False
+        for ch in self.bot.ticket_data:
+            if ctx.channel.id == ch['ID']:
+                okay_to_delete = True
+
+        if okay_to_delete == False:
+            if self.bot.use_timestamp:
+                embed = discord.Embed(
+                    title = "Failed to Close Ticket",
+                    description = "This command can only be used inside a channel for a ticket.",
+                    color = random.choice(self.bot.embed_colors),
+                    timestamp = datetime.datetime.now(datetime.timezone.utc)
+                )
+            else:
+                embed = discord.Embed(
+                    title = "Failed to Close Ticket",
+                    description = "This command can only be used inside a channel for a ticket.",
+                    color = random.choice(self.bot.embed_colors),
+                    timestamp = datetime.datetime.now(datetime.timezone.utc)
+                )
+            if self.bot.show_command_author:
+                embed.set_author(
+                    name = ctx.author.name,
+                    icon_url = ctx.author.avatar_url
+                )
+            embed.set_footer(
+                text = self.bot.footer_text,
+                icon_url = self.bot.footer_icon
+            )
+            await ctx.send(embed = embed)
+        else:
+            channel = ctx.channel
+            author = ctx.author
+
+            def check(m):
+                if m.content.startswith("{prefix}confirm".format(prefix = self.bot.prefix)) and m.author == author and m.channel == channel:
+                    return True
+                else:
+                    return False
+
+            if self.bot.use_timestamp:
+                embed = discord.Embed(
+                    title = 'Are you sure?',
+                    description = "Once confirmed, you cannot cancel this.\nTo confirm, type `{prefix}confirm`.\n*You have 10 seconds to respond.*".format(prefix = self.bot.prefix),
+                    color = random.choice(self.bot.embed_colors),
+                    timestamp = datetime.datetime.now(datetime.timezone.utc)
+                )
+            else:
+                embed = discord.Embed(
+                    title = 'Are you sure?',
+                    description = "Once confirmed, you cannot cancel this.\nTo confirm, type `{prefix}confirm`.\n*You have 10 seconds to respond.*".format(prefix = self.bot.prefix),
+                    color = random.choice(self.bot.embed_colors)
+                )
+            if self.bot.show_command_author:
+                embed.set_author(
+                    name = ctx.author.name,
+                    icon_url = ctx.author.avatar_url
+                )
+            embed.set_footer(
+                text = self.bot.footer_text,
+                icon_url = self.bot.footer_icon
+            )
+            msg = await ctx.send(embed = embed)
+
+            try:
+                msg = await self.bot.wait_for('message', check = check, timeout = 10.0)
+
+            except asyncio.TimeoutError:
+                if self.bot.use_timestamp:
+                    embed = discord.Embed(
+                        title = "Closing Cancelled",
+                        color = random.choice(self.bot.embed_colors),
+                        timestamp = datetime.datetime.now(datetime.timezone.utc)
+                    )
+                else:
+                    embed = discord.Embed(
+                        title = "Closing Cancelled",
+                        color = random.choice(self.bot.embed_colors)
+                    )
+                if self.bot.show_command_author:
+                    embed.set_author(
+                        name = ctx.author.name,
+                        icon_url = ctx.author.avatar_url
+                    )
+                embed.set_footer(
+                    text = self.bot.footer_text,
+                    icon_url = self.bot.footer_icon
+                )
+                await msg.edit(embed = embed)
+
+            else:
+                if self.bot.log_tickets:
+                    for ticket in self.bot.ticket_data:
+                        if ctx.channel.id == ticket['ID']:
+                            try:
+                                user_ob = await self.bot.fetch_user(ticket['UserID'])
+                                if self.bot.use_timestamp:
+                                    embed = discord.Embed(
+                                        title = "Here is the record of your ticket in {}.".format(ctx.guild.name),
+                                        color = random.choice(self.bot.embed_colors),
+                                        timestamp = datetime.datetime.now(datetime.timezone.utc)
+                                    )
+                                else:
+                                    embed = discord.Embed(
+                                        title = "Here is the record of your ticket in {}.".format(ctx.guild.name),
+                                        color = random.choice(self.bot.embed_colors)
+                                    )
+                                embed.set_footer(
+                                    text = self.bot.footer_text,
+                                    icon_url = self.bot.footer_icon
+                                )
+                                await user_ob.send(content = "Here is the record of your ticket in {}.".format(ctx.guild.name), file = discord.File(self.bot.ticket_folder + '/{}.txt'.format(str(ctx.channel.id))))
+                                os.remove(self.bot.ticket_folder + '/{}.txt'.format(str(ctx.channel.id)))
+                            except:
+                                os.remove(self.bot.ticket_folder + '/{}.txt'.format(str(ctx.channel.id)))
+
+                for ticket in self.bot.ticket_data:
+                    if ticket['ID'] == ctx.channel.id:
+                        self.bot.ticket_data.remove(ticket)
+
+                with open(self.bot.ticket_file, 'w+') as file:
+                    file.write(json.dumps(self.bot.ticket_data, indent = 4, sort_keys = True))
+
+                await ctx.channel.delete(reason = 'Ticket Closed')
+
+    @commands.command(name = 'rename', aliases = ['rename-ticket', 'rename_ticket'], help = "Set the name of the ticket")
+    @commands.guild_only()
+    async def rename_ticket(self, ctx, *, name = None):
+        if self.bot.delete_commands:
+            await ctx.message.delete()
+        if name == None:
+            if self.bot.use_timestamp:
+                embed = discord.Embed(
+                    title = "Failed to Rename Ticket",
+                    description = "You must include a new ticket name.",
+                    color = random.choice(self.bot.embed_colors),
+                    timestamp = datetime.datetime.now(datetime.timezone.utc)
+                )
+            else:
+                embed = discord.Embed(
+                    title = "Failed to Rename Ticket",
+                    description = "You must include a new ticket name.",
+                    color = random.choice(self.bot.embed_colors)
+                )
+            if self.bot.show_command_author:
+                embed.set_author(
+                    name = ctx.author.name,
+                    icon_url = ctx.author.avatar_url
+                )
+            embed.set_footer(
+                text = self.bot.footer_text,
+                icon_url = self.bot.footer_icon
+            )
+            await ctx.send(embed = embed)
+        elif len(name) < 1 or len(name) > 100:
+            if self.bot.use_timestamp:
+                embed = discord.Embed(
+                    title = "Failed to Rename Ticket",
+                    description = "New ticet name must be between 1 and 100 characters, {} characters is an invalid number.".format(len(name)),
+                    color = random.choice(self.bot.embed_colors),
+                    timestamp = datetime.datetime.now(datetime.timezone.utc)
+                )
+            else:
+                embed = discord.Embed(
+                    title = "Failed to Rename Ticket",
+                    description = "New ticet name must be between 1 and 100 characters, {} characters is an invalid number.".format(len(name)),
+                    color = random.choice(self.bot.embed_colors)
+                )
+            if self.bot.show_command_author:
+                embed.set_author(
+                    name = ctx.author.name,
+                    icon_url = ctx.author.avatar_url
+                )
+            embed.set_footer(
+                text = self.bot.footer_text,
+                icon_url = self.bot.footer_icon
+            )
+            await ctx.send(embed = embed)
+        else:
+            name = name.replace(" ", "-").lower()
+            await ctx.channel.edit(name = re.sub(r'^\w-', '', name))
+
+def setup(bot):
+    global support_channel
+    with open("./Config.yml", 'r') as file:
+        support_channel = yaml.load(file, Loader = yaml.Loader)['Tickets']['Support Channel ID']
+    bot.add_cog(TicketsCog(bot))
---
+++ DiscordTickets.py
@@ -0,0 +1,136 @@
+import discord
+from discord.ext import commands
+import datetime
+import yaml
+import os
+import sys
+import random
+import json
+
+with open("./Config.yml", 'r') as file:
+    config = yaml.load(file, Loader = yaml.Loader)
+
+bot = commands.AutoShardedBot(command_prefix=config['Prefix'], description="Heroicos_HM's Tickets Bot", case_insensitive = True)
+bot.remove_command('help')
+
+#Main Config
+bot.TOKEN = config['TOKEN']
+bot.prefix = config['Prefix']
+bot.logs_channels = config['Log Channels']
+bot.embed_colors = config['Embed Colors']
+bot.footer_icon = config['Footer Icon URL']
+bot.footer_text = config['Footer Text']
+bot.online_message = config['Online Message']
+
+#Options
+bot.use_timestamp = config['Options']['Embed Timestamp']
+bot.delete_commands = config['Options']['Delete Commands']
+bot.show_command_author = config['Options']['Show Author']
+bot.show_game_status = config['Options']['Game Status']['Active']
+bot.game_to_show = config['Options']['Game Status']['Game']
+
+#Ticket Config
+bot.support_role_name = config['Tickets']['Support Role Name']
+bot.ticket_folder = config['Tickets']['Ticket Log Folder']
+if not os.path.exists(bot.ticket_folder):
+    os.mkdir(os.path.abspath(bot.ticket_folder))
+bot.ticket_file = config['Tickets']['Data File']
+if os.path.isfile(bot.ticket_file):
+    with open(bot.ticket_file, 'r') as file:
+        bot.ticket_data = json.loads(file.read())
+        if bot.ticket_data == None:
+            bot.ticket_data = []
+else:
+    bot.ticket_data = []
+bot.log_tickets = config['Tickets']['Log All Tickets']
+bot.support_channel_id = config['Tickets']['Support Channel ID']
+
+extensions = [
+    'Cogs.General',
+    'Cogs.Tickets'
+]
+
+for extension in extensions:
+    bot.load_extension(extension)
+
+@bot.event
+async def on_ready():
+    print('Logged in as {0} and connected to Discord! (ID: {0.id})'.format(bot.user))
+    if bot.show_game_status:
+        game = discord.Game(name = bot.game_to_show.format(prefix = bot.prefix))
+        await bot.change_presence(activity = game)
+    if bot.use_timestamp:
+        embed = discord.Embed(
+            title = bot.online_message.format(username = bot.user.name),
+            color = random.choice(bot.embed_colors),
+            timestamp = datetime.datetime.now(datetime.timezone.utc)
+        )
+    else:
+        embed = discord.Embed(
+            title = bot.online_message.format(username = bot.user.name),
+            color = random.choice(bot.embed_colors)
+        )
+    embed.set_footer(
+        text = bot.footer_text,
+        icon_url = bot.footer_icon
+    )
+    for log in bot.logs_channels:
+        channel = bot.get_channel(log)
+        await channel.send(content = None, embed = embed)
+
+    bot.start_time = datetime.datetime.now(datetime.timezone.utc)
+
+@bot.command(name='help')
+@commands.guild_only()
+async def dfs_help(ctx):
+    if bot.delete_commands:
+        await ctx.message.delete()
+    if bot.use_timestamp:
+        embed = discord.Embed(
+            title = ":newspaper: Help",
+            color = random.choice(bot.embed_colors),
+            timestamp = datetime.datetime.now(datetime.timezone.utc)
+        )
+    else:
+        embed = discord.Embed(
+            title = ":newspaper: Help",
+            color = random.choice(bot.embed_colors)
+        )
+    if bot.show_command_author:
+        embed.set_author(
+            name = ctx.author.name,
+            icon_url = ctx.author.avatar_url
+        )
+    embed.add_field(
+        name = bot.prefix + "new <reason>",
+        value = "Creates a support ticket for the given reason.\nCan only be used in {mention}.".format(mention = bot.get_channel(bot.support_channel_id).mention),
+        inline = False
+    )
+    embed.add_field(
+        name = bot.prefix + "close",
+        value = "Closes a ticket. Can only be used in a ticket channel.",
+        inline = False
+    )
+    embed.add_field(
+        name = bot.prefix + "rename",
+        value = "Renames a ticket. Can only be used in a ticket channel.",
+        inline = False
+    )
+    embed.add_field(
+        name = bot.prefix + "uptime",
+        value = "Returns the amount of time that the bot has been online.",
+        inline = False
+    )
+    embed.add_field(
+        name = bot.prefix + "ping",
+        value = "Gets the ping times from the bot to the discord servers and back.",
+        inline = False
+    )
+    embed.set_footer(
+        text = bot.footer_text,
+        icon_url = bot.footer_icon
+    )
+
+    await ctx.send(embed = embed)
+
+bot.run(bot.TOKEN, bot = True, reconnect = True)
